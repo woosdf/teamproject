9c2de270cd10f8c53bb1bdd6b8788df309147812
woosdf <kacaw00d@naver.com>
2022-03-19 23:23:56 +0900

    login activity 구현

---
+++ app/src/main/java/com/example/project3/SplashActivity.java
@@ -0,0 +1,29 @@
+package com.example.project3;
+
+import android.content.Intent;
+import android.os.Handler;
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+        //로딩화면 시작.
+        Loadingstart();
+    }
+    private void Loadingstart(){
+        Handler handler=new Handler();
+        handler.postDelayed(new Runnable(){
+            public void run(){
+                Intent intent=new Intent(getApplicationContext(),LoginActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        },2000);
+    }
+}
+
